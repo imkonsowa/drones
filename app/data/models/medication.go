c0805a1d9d58424a93c9e4f326eb46e82d729019
Ibrahim Konsowa <imkonsowa@gmail.com>
2022-04-06 01:59:42 +0200

    fix medication image URL path after find.

--- app/data/models/medication.go
+++ app/data/models/medication.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"gorm.io/gorm"
+	"strings"
+)
+
 type Medication struct {
 	AppModel
 	DroneSerialNumber string `json:"drone_serial_number"` // TODO: add foreign key
@@ -11,4 +16,11 @@ type Medication struct {
 
 func (*Medication) TableName() string {
 	return "medications"
+}
+
+func (m *Medication) AfterFind(tx *gorm.DB) (err error) {
+	if len(m.ImageURL) > 0 && !strings.HasPrefix(m.ImageURL, "/") {
+		m.ImageURL = "/" + m.ImageURL
+	}
+	return
 }
